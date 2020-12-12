4def3a0505ab20df69685bb14dd34b7a68cca0ee
Uherhsen <tovaris4uherhsen@yandex.ru>
2020-12-12 16:56:31 +0300

    с. 24 Райан митчел Скрапинг веб сайтов.

---
+++ scrapetest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 16:36:22 2020
+
+Учебник Райан Митчелл Скрапинг Веб-сайтов с помощью Python
+c. 23
+"""
+
+from urllib.request import urlopen
+html = urlopen("http://pythonscraping.com/pages/page1.html")
+print(html.read())
