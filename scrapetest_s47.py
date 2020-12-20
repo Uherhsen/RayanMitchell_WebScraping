762b5010837d2d8aa63ca671055a6abdf6353610
Uherhsen <tovaris4uherhsen@yandex.ru>
2020-12-20 17:12:19 +0300

    Регулярные выражения с 47

---
+++ scrapetest_s47.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 20 16:35:46 2020
+
+Регулярные выражения с 47
+для обучения используется:
+    http://bit.ly/1KGe2Qk оно же (http://www.pythonscraping.com/pages/page3.html)
+    
+Получить доступ к питоновскому списку атрибутов можно ввтоматически , вызвав 
+myTag.attrs
+Например адрес файла изображения можно найти с помощью следующей строки 
+myTag.attrs['src']
+"""
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html, features="html.parser")
+images = bsObj.findAll("img",{"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+for image in images:
+    print(image["src"])
+    
